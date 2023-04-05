a3fa5de504b17d76d3787a7a54303c39194be7a7
susuje <wjdtn0n@naver.com>
2023-04-05 21:00:26 +0900

    Basic of JS

--- 2주차/4.4/002_Array_2.js
+++ 2주차/4.4/002_Array_2.js
@@ -5,15 +5,6 @@ arr.filter(function(el){
     return el % 2 === 0
 })
 
-/// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
-///프로그래머스 코테 레벨0! 
-function solution(n) {
-    return Array(n)
-        .fill()
-        .map((_, i) => i + 1) //아이템자리에 _를 사용. 의미업는값이니까용
-        .filter((v) => v % 2 === 0)
-        .reduce((a, c) => a + c, 0);
-}
 
 //홀수
 const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -28,6 +19,19 @@ Array.from('hello world').filter(v => !["a", "e", "i", "o", "u"].includes(v))
 // reduce , 0을항상 넣어주세용
 const arr3 = [1, 2, 3, 4, 5, 6]
 arr3.reduce((a,c)=> a+c, 0) //0은 초기값
+
+
+
+/// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+///프로그래머스 코테 레벨0! 
+function solution(n) {
+    return Array(n)
+        .fill()
+        .map((_, i) => i + 1) //아이템자리에 _를 사용. 의미업는값이니까용
+        .filter((v) => v % 2 === 0)
+        .reduce((a, c) => a + c, 0);
+}
+
 
 // includes
 const arr1 = ['hello', 'world', 'hojun']
